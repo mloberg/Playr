e6d1d4c348c42e3d78a79a7c96d9dfb70bcba66a
Matthew Loberg <loberg.matt@gmail.com>
2011-11-23 13:28:48 -0600

    auth class wip

--- app.rb
+++ app.rb
@@ -5,7 +5,8 @@ require './auth'
 #require 'rack-flash'
 #require 'sinatra/redirect_with_flash'
 
-use Rack::Session::Pool, :expire_after => 2592000
+#use Rack::Session::Pool, :expire_after => 2592000
+enable :sessions
 #use Rack::Flash, :sweep => true
 
 #SITE_TITLE = ""
@@ -13,35 +14,31 @@ use Rack::Session::Pool, :expire_after => 2592000
 
 set(:auth) do |val|
 	condition do
-		redirect '/login' unless @auth and @auth.valid?
+		redirect '/login' unless @auth and @auth.is_valid?
 	end
 end
 
 before do
-	SESS = session
+	$_SERVER = request.env
+	$_SESSION = session
 	if session[:user_id]
 		@user = User.get session[:user_id]
 		@auth = Auth.new(@user.password, @user.secret)
-	else
-		
 	end
 end
 
 get "/", :auth => true do
 	#UserAuth.crypt "admin"
-	"authenticated!"
 end
 
-get '/info' do
-	session[:user_id].to_s
-end
-
-get '/clear' do
+# Login/Logout
+get '/logout' do
 	session[:user_id] = nil
+	session[:fingerprint] = nil
 end
 
 get "/login" do
-	if @auth and @auth.valid?
+	if @auth and @auth.is_valid?
 		redirect '/'
 	else
 		erb :login
@@ -52,5 +49,7 @@ post "/login" do
 	username = params[:username]
 	user = User.first(:username => username)
 	session[:user_id] = user.id
+	@auth = Auth.new(user.password, user.secret)
+	Auth.validate
 	redirect '/'
 end
--- auth.rb
+++ auth.rb
@@ -1,23 +1,25 @@
 require 'bcrypt'
+require 'digest/md5'
 
 class Auth
 
-	# class methods
-	
-	def self.logged_in?
-		return false unless SESS[:user_id]
-		
-	end
-	
-	# instance methods
-	
-	def initialize(pass, secret)
+	def initialize(pass, hased, secret)
 		@pass = pass
 		@secret = secret
 	end
 	
-	def valid?
-		return true if @secret
+	def validate
+		fingerprint = BCrypt::Engine.hash_secret(Digest::MD5.hexdigest($_SERVER['HTTP_USER_AGENT']) + @secret, BCrypt::Engine.generate_salt)
+		$_SESSION[:fingerprint] = fingerprint
+	end
+	
+	def is_valid?
+		calculated_fingerprint = BCrypt::Engine.hash_secret(Digest::MD5.hexdigest($_SERVER['HTTP_USER_AGENT']) + @secret, BCrypt::Engine.generate_salt)
+		if $_SESSION[:fingerprint] == calculated_fingerprint
+			return true
+		else
+			return false
+		end
 	end
 
 end
